ec89c25550814c96c1703ad012bdcd4a5b8712c1
ikemal12 <iltar2005@gmail.com>
2025-12-11 20:43:20 +0000

    feat: implement getters for top of book

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,5 +1,19 @@
 #include "orderbook.hpp"
 
+std::optional<Order> OrderBook::bestBid() const {
+    if (bids.empty()) {
+        return std::nullopt;
+    }
+    return *bids.begin();
+}
+
+std::optional<Order> OrderBook::bestAsk() const {
+    if (asks.empty()) {
+        return std::nullopt;
+    }
+    return *asks.begin();
+}
+
 std::multiset<Order>::iterator OrderBook::findOrder(std::multiset<Order>& book, int orderId) {
     for (auto it = book.begin(); it != book.end(); ++it) {
         if (it->id == orderId) {
